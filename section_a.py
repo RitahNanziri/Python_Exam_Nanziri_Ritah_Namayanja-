d6c2c23bcc8ed8f8809afd8c070b89425324f9ee
RitahNanziri <142292629+RitahNanziri@users.noreply.github.com>
2024-05-02 12:22:37 +0300

    Add files via upload

---
+++ section_a.py
@@ -0,0 +1,54 @@
+# a,(i)
+
+# mark=int(input("enter mark"))
+# def grade_students():
+#     if mark >=90 and mark <=100:
+#         print("A")  
+#     elif mark>=80 and mark <=89:
+#         print("B")
+#     elif mark>=70 and mark <=79:
+#         print("C")  
+#     elif mark>60 and mark <=69:  
+#         print("D")
+#     elif mark>50 and mark<=59: 
+#         print("E")        
+#     else:
+#         print("Fail")    
+# grade_students()     
+
+# mark=85
+
+
+
+# ii,
+# def fahrenheit_to_celsius(fahrenheit):
+#     celsius = (fahrenheit - 32) * 5/9
+#     return celsius
+
+# fahrenheit = 56
+# celsius = fahrenheit_to_celsius(fahrenheit)
+# print(f"{fahrenheit} degrees Fahrenheit is equal to {celsius:.2f} degrees Celsius.")
+
+
+
+# b,(i)
+
+# def calculate_area(base, height):
+#     area = 0.5 * base * height
+#     return area
+
+# base = 2
+# height = 3
+# area = calculate_area(base, height)
+# print(f"The area of the triangle is {area}")
+
+
+
+
+
+
+# ii
+# my_list = [9, 2, 3, 5, 8]
+# x = sum(my_list)
+# print(x)
+
